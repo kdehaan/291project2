b81c1c072c1d01c0c37528ae9d271bda6e17b0a3
Kevin de Haan <kbehaan@gmail.com>
2017-11-23 18:29:53 -0700

    python regex is strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-
+import re
 
 def parse_data():
 
@@ -17,10 +17,48 @@ def parse_data():
     #         data = sys.stdin
     #         break
 
-    data = sys.stdin.readlines()
+    data = sys.stdin.read()
     # example usage on linux: cat 10.txt | python3 main.py
 
     return data
+
+
+def prepare_files(data):
+    create_terms(data)
+    create_years(data)
+    create_recs(data)
+
+
+def create_terms(data):
+    # articles = re.compile('<article.*/article>')
+    # result = articles.match(data)
+
+    line = "Cats are smarter than dogs";
+
+    searchObj = re.search(r'(.*)a(.*?) .*', data, re.M | re.I)
+
+    if searchObj:
+        print(searchObj.group())
+        for item in searchObj.groups():
+            print(item)
+
+
+    # regex:
+    # article block: <article.*/article>
+    # article key: (?<=key=\").*(?=\")
+    # terms: [0-9a-zA-Z_]{3,}
+    # authors: (?<=<author>).*?(?=</)
+    # title: (?<=<title>).*?(?=</)
+    # contained tags with titles (?=(<[\w]+>)).*?(?=</)
+    return
+
+
+def create_years(data):
+    return
+
+
+def create_recs(data):
+    return
 
 
 def setup():
@@ -31,8 +69,7 @@ def setup():
 
 def main():
     data = parse_data()
-    for line in data:
-        print(line)
+    prepare_files(data)
 
 
 if __name__ == "__main__":
