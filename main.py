7617c84c9b75272a8fae957a3b67fa85a79d9dd6
Kevin de Haan <kbehaan@gmail.com>
2017-11-26 21:43:05 -0700

    split into part2

--- main.py
+++ main.py
@@ -2,53 +2,7 @@ import subprocess
 from bsddb3 import db
 import re
 
-def fill_db(database, curs, filename):
-    #outstr = '-o' + filename
-    sort = subprocess.Popen(['sort', '-u', filename], stdout=subprocess.PIPE)
-    for line in sort.stdout:
-        key = ''
-        value = ''
-        if line[0] == 10:
-            pass
-        else:
-            keyfound = False
-            count = -1
-            while not keyfound:
-                count += 1
-                if line[count] == 58:
-                    keyfound = True
-                else:
-                    key += chr(line[count])
-            valuefound = False
-            while not valuefound:
-                count += 1
-                if line[count] == 10:
-                    valuefound = True
-                else:
-                    value += chr(line[count])
-            key = key.encode('utf-8')
-            database.put(key, value)
 
-def uncaps(answer):
-    count = 0
-    for i in answer:
-        if 64 < ord(i) < 91:
-            answer[count] = chr(ord(i) + 32)
-        count += 1
-    return answer
-
-def print_db(db):
-    curs = db.cursor()
-    iter_curs = curs.first()
-    while iter_curs:
-        print(iter_curs)
-        iter_curs = curs.next()
-
-def clear_db(database, curs):
-    iter = curs.first()
-    while (iter):
-        curs.delete()
-        iter = curs.first()
 
 def get_queries():
     sel = input("Enter your database search. To see query options, enter h. To quit, enter q: ").lower()
@@ -60,24 +14,7 @@ def get_queries():
     return queries
 
 def main():
-    terms = db.DB()
-    terms.open("terms.idx", None, db.DB_BTREE, db.DB_CREATE)
-    termscur = terms.cursor()
-    years = db.DB()
-    years.open("years.idx", None, db.DB_BTREE, db.DB_CREATE)
-    yearscur = years.cursor()
-    recs = db.DB()
-    recs.open("recs.idx", None, db.DB_HASH, db.DB_CREATE)
-    recscur = recs.cursor()
 
-    clear = uncaps(input("Would you like to clear the databases and remake them, or use the existing databases? [Y/N] "))
-    if clear == 'y':
-        clear_db(recs, recscur)
-        clear_db(years, yearscur)
-        clear_db(terms, termscur)
-        fill_db(terms, termscur, 'terms.txt')
-        fill_db(years, yearscur, 'years.txt')
-        fill_db(recs, recscur, 'recs.txt')
 
     done = False
     output = key
@@ -154,12 +91,6 @@ def main():
 
 
 
-    terms.close()
-    years.close()
-    recs.close()
-    termscur.close()
-    yearscur.close()
-    recscur.close()
 
 
 
