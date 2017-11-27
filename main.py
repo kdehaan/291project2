cc3339c07c085e037f4ace436ec76638a399a445
Kevin de Haan <kbehaan@gmail.com>
2017-11-27 09:54:18 -0700

    more search term parsing

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     match_title = re.compile(r"title:")
     match_author = re.compile(r"author:")
     match_year = re.compile(r"year[:<>]")
-    match_other = re.compile(r"other:")
+    match_other = re.compile(r":")
 
     while True:
         terms = db.DB()
@@ -37,30 +37,48 @@ def main():
                 print("changed output (key)")
             elif item == "output=full":
                 output_type = 'full'
-                print("changed output")
+                print("changed output (full)")
             elif match_title.match(item):
                 if '"' in item:
-                    print("multival")
                     search_term = item[7:-1]
-                    print(search_term)
+                    search_terms = search_term.split()
+                    for term in search_terms:
+                        term = 't-' + term
+                        print(term)
                 else:
                     search_term = item[6:]
+                    search_term = 't-' + search_term
                     print(search_term)
             elif match_author.match(item):
                 search_term = item[7:]
+                search_term = 'a-' + search_term
                 print(search_term)
             elif match_year.match(item):
                 equality = item[4]
                 search_term = item[5:]
+                if equality == ':':
+                    continue
+                if equality == '<':
+                    continue
+                if equality == '>':
+                    continue
                 print(search_term)
-            elif match_other.match(item):
-                search_term = item[6:]
+            elif match_other.search(item):
+                search_term = item.split(':')
+                search_term = search_term[1]
+                search_term = 'o-' + search_term
                 print(search_term)
             else:
                 search_term = item
                 print(item)
 
 
+        terms.close()
+        years.close()
+        recs.close()
+        termscur.close()
+        yearscur.close()
+        recscur.close()
 
 if __name__ == "__main__":
     main()
