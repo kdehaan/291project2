7f9be3aad7cab44391bd29b3604b694bd9347340
Kevin de Haan <kbehaan@gmail.com>
2017-11-27 13:32:50 -0700

    years still not implemented

--- main.py
+++ main.py
@@ -12,6 +12,20 @@ def get_queries():
         queries.append(item.group())
     return queries
 
+def get_year(yearscur, year, qualifier):
+    result = yearscur.get
+
+def get_data(termscur, search_term):
+    result = termscur.get(search_term, search_term, db.DB_SET)
+    curs_iter = result
+    result_set = set()
+    while curs_iter[0] == search_term:
+        result_set.add(curs_iter[1].decode('utf-8'))
+        curs_iter = termscur.next()
+        if not curs_iter:
+            curs_iter = ['']
+    return result_set
+
 def main():
     output_type = 'key'
     match_title = re.compile(r"title:")
@@ -32,6 +46,8 @@ def main():
         recscur = recs.cursor()
         queries = get_queries()
 
+
+        set_list = list()
         for item in queries:
             if item == "output=key":
                 output_type = 'key'
@@ -45,40 +61,32 @@ def main():
                     search_terms = search_term.split()
                     for term in search_terms:
                         term = 't-' + term
-                        print(term)
+                        term = term.encode('utf-8')
+                        set_list.append(get_data(termscur, term))
                 else:
                     search_term = item[6:]
                     search_term = 't-' + search_term
                     search_term = search_term.encode('utf-8')
-                    result = termscur.get(search_term, search_term, db.DB_SET)
-                    curs_iter = result
-                    while curs_iter[0] == search_term:
-                        print(curs_iter)
-                        curs_iter = termscur.next()
-                        if not curs_iter:
-                            curs_iter = ['']
+                    set_list.append(get_data(termscur, search_term))
             elif match_author.match(item):
                 search_term = item[7:]
                 search_term = 'a-' + search_term
-                print(search_term)
+                set_list.append(get_data(termscur, search_term))
             elif match_year.match(item):
                 equality = item[4]
                 search_term = item[5:]
-                if equality == ':':
-                    continue
-                if equality == '<':
-                    continue
-                if equality == '>':
-                    continue
-                print(search_term)
+                search_term = search_term.encode('utf-8')
+                set_list.append(get_year(yearscur, search_term, equality))
             elif match_other.search(item):
                 search_term = item.split(':')
                 search_term = search_term[1]
                 search_term = 'o-' + search_term
-                print(search_term)
+                set_list.append(get_data(termscur, search_term))
             else:
-                print("general term found")
+                search_term = item
+                set_list.append(get_data(termscur, search_term))
 
+        print(set_list)
 
 
 
